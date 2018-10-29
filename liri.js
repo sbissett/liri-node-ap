488d30ac870423a37ef2dfcb6fc934deb384d093
Stephen Bissett <steve.bissett5@gmail.com>
2018-10-29 11:36:12 -0600

    with spotify

--- liri.js
+++ liri.js
@@ -41,3 +41,44 @@ function commands (liriCommand, input){
       console.log("Not working. Enter one of the following commands: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says' followed by parameter.");
     }
 }
+
+//function for each liri command
+
+// Function for spotify
+
+
+//Function for Spotify
+function getSong(songName) {
+    var spotify = new Spotify(keys.spotify);
+
+    //If no song is provided, use "The Sign" 
+        if (!songName) {
+            songName = "The Sign";
+        };        
+
+        console.log(songName);
+
+        //Callback to spotify to search for song name
+        spotify.search({ type: 'track', query: songName}, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            } 
+            console.log("Artist: " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name +
+            "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].preview_url); 
+            
+            //Creates a variable to save text into log.txt file
+            var logSong = "Artist: " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name +
+            "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].preview_url + "\n";
+            
+            //Appends text to log.txt file
+            fs.appendFile('log.txt', logSong, function (err) {
+                if (err) throw err;
+              });
+            
+            logResults(data);
+        });
+};
+
+
+
+
