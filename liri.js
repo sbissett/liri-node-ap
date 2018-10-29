30adda896e57c7e544d4c791d9ca9554f61b4d18
Stephen Bissett <steve.bissett5@gmail.com>
2018-10-29 11:24:04 -0600

    with switch statements

--- liri.js
+++ liri.js
@@ -7,3 +7,37 @@ var keys = require('./keys.js');
 var request = require('request');
 var twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+
+//Variables to target specific APIs in the keys.js file
+
+var liricommand = process.argv[2];
+var input = process.argv[3];
+
+// Available commands for liri
+//my tweets, spotify-this-song, do-what-it-says
+
+// Available commands for liri 
+//my-tweets, spotify-this-song, movie-this, do-what-it-says
+
+function commands (liriCommand, input){
+
+    switch (liriCommand) {
+        case "my-tweets":
+        getTweets(input);
+        break; case "spotify-this-song":
+    getSong(input);
+    break;
+
+    case "movie-this":
+    getMovie(input);
+    break;
+
+    case "do-what-it-says":
+    getRandom();
+    break;
+
+    //If no command is entered, this is the default message to user
+    default:
+      console.log("Not working. Enter one of the following commands: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says' followed by parameter.");
+    }
+}
