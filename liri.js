b3b3d88c036a190ab7416ecb244acdd39d717be1
Stephen Bissett <steve.bissett5@gmail.com>
2018-10-29 11:47:18 -0600

    with movies

--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 var fs = require("fs");
 var keys = require('./keys.js');
 var request = require('request');
-var twitter = require('twitter');
+var twitter = require('node-concert-api');
 var Spotify = require('node-spotify-api');
 
 //Variables to target specific APIs in the keys.js file
@@ -17,28 +17,31 @@ var input = process.argv[3];
 //my tweets, spotify-this-song, do-what-it-says
 
 // Available commands for liri 
-//my-tweets, spotify-this-song, movie-this, do-what-it-says
+//concert-this, spotify-this-song, movie-this, do-what-it-says
 
 function commands (liriCommand, input){
 
     switch (liriCommand) {
-        case "my-tweets":
+
+        case "concert-this":
         getTweets(input);
-        break; case "spotify-this-song":
-    getSong(input);
-    break;
+        break; 
+        
+        case "spotify-this-song":
+        getSong(input);
+        break;
 
-    case "movie-this":
-    getMovie(input);
-    break;
+        case "movie-this":
+        getMovie(input);
+        break;
 
-    case "do-what-it-says":
-    getRandom();
-    break;
+        case "do-what-it-says":
+        getRandom();
+        break;
 
     //If no command is entered, this is the default message to user
     default:
-      console.log("Not working. Enter one of the following commands: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says' followed by parameter.");
+      console.log("Not working. Enter one of the following commands: 'concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says' followed by parameter.");
     }
 }
 
@@ -82,3 +85,51 @@ function getSong(songName) {
 
 
 
+//Function for movies
+
+function getMovie(movieName) {
+    //If no movie name is provided, use Mr.Nobody as default
+        if (!movieName) {
+            movieName = "mr nobody";
+        }
+            
+    // Runs a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json&tomatoes=true&apikey=trilogy";
+
+    // Helps debugging
+    console.log(queryUrl);
+
+    //Callback to OMDB API to get movie info
+    request(queryUrl, function(error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            var movieObject = JSON.parse(body);
+
+            //console.log(movieObject); // Show the text in the terminal
+            var movieResults = 
+            "------------------------------ begin ------------------------------" + "\r\n" +
+            "Title: " + movieObject.Title+"\r\n"+
+            "Year: " + movieObject.Year+"\r\n"+
+            "Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+            "Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+            "Country: " + movieObject.Country+"\r\n"+
+            "Language: " + movieObject.Language+"\r\n"+
+            "Plot: " + movieObject.Plot+"\r\n"+
+            "Actors: " + movieObject.Actors+"\r\n"+
+            "------------------------------ end ------------------------------" + "\r\n";
+            console.log(movieResults);
+
+            //Appends movie results to log.txt file
+            fs.appendFile('log.txt', movieResults, function (err) {
+                if (err) throw err;
+              });
+              console.log("Saved!");
+              logResults(response);
+        } 
+        else {
+			console.log("Error :"+ error);
+			return;
+		}
+    });
+};
