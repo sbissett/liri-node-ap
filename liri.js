7c64531d97c720d7e0d8b2e90a6aab95a5fc1006
Stephen Bissett <steve.bissett5@gmail.com>
2018-10-29 11:11:20 -0600

    InitialCode

--- liri.js
+++ liri.js
@@ -1,2 +1,9 @@
 // Code to read and set any environment variables with the dotenv package
 require("dotenv").config();
+
+// Importing files needed to run the funtions
+var fs = require("fs");
+var keys = require('./keys.js');
+var request = require('request');
+var twitter = require('twitter');
+var Spotify = require('node-spotify-api');
